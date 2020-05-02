37afed75ef91e3449c57e5cf1d9a84ed6ef651e7
DevCatsi <jus.sobcz@gmail.com>
2020-05-02 21:39:04 +0200

    Finished section template.

--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -15,11 +15,22 @@ class Section extends React.Component {
         return (
             <React.Fragment>
                 <h2 className='task'>Use the alphabet below to guess the word, or click hint to get a clue.</h2>
-                <section className='sectionContainer'>
-                    <div className='alphabetPosition'>
+                <section className='game-container'>
+                    <div className='alphabet-position'>
                         {btnLetters}
                     </div>
                     <div className='inquiry'>inquiry</div>
+                    <div className='answer-area'>Answer area _ _ _</div>
+                    <div className='life-qty'>
+                        <p className='text-information'>You have /number/ lives</p>
+                        <p className='clue'>Clue:</p>
+                        <div className='draw-information'>Draw</div>
+                    </div>
+                    <div className='btn-position'>
+                        <button className='btn'>Hint</button>
+                        <button className='btn'>Play again!</button>
+                    </div>
+
                 </section>
             </React.Fragment>
         )
