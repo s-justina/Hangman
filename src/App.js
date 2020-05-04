aeed02b0c8130ecb08bf6cdd887a501c4ef2acf5
DevCatsi <jus.sobcz@gmail.com>
2020-05-04 22:22:52 +0200

    Create footer.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import './Components/Header/Header';
 import Header from "./Components/Header/Header";
 import Section from "./Components/Section/Section";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
     return (
         <div className="App">
             <Header className='topHeader'/>
             <Section className='bottomSection'/>
+            <Footer/>
         </div>
     );
 }
